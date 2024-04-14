a9cc3cef7494972d2e26aa3cd4680f5089e952b3
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-04-14 12:38:02 -0400

    feat: add mapper from user to credential dto

--- Src/Helpers/AutoMapperProfiles.cs
+++ Src/Helpers/AutoMapperProfiles.cs
@@ -1,8 +1,13 @@
 using AutoMapper;
+using courses_dotnet_api.Src.DTOs.Account;
+using courses_dotnet_api.Src.Models;
 
 namespace courses_dotnet_api.Src.Helpers;
 
 public class AutoMapperProfiles : Profile
 {
-    public AutoMapperProfiles() { }
+    public AutoMapperProfiles()
+    {
+        CreateMap<User, CredentialDto>();
+    }
 }
