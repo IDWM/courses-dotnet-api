8f417f03310b1244ad2359fa5dbcfe73804c7066
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-04-15 15:17:27 -0400

    feat: implement methods

--- Src/Controllers/CourseController.cs
+++ Src/Controllers/CourseController.cs
@@ -13,14 +13,43 @@ public class CourseController(ICourseRepository courseRepository) : BaseApiContr
     private readonly string[] _roles = ["admin", "teacher"];
 
     [HttpPost]
-    public async Task<IResult> CreateCourse(CreateCourseDto createCourseDto) { }
+    public async Task<IResult> CreateCourse(CreateCourseDto createCourseDto)
+    {
+        if (await _courseRepository.CourseExistsByNameAsync(createCourseDto.Name))
+        {
+            return TypedResults.BadRequest("Course already exists");
+        }
+
+        await _courseRepository.AddCourseAsync(createCourseDto);
+        await _courseRepository.SaveChangesAsync();
+        return TypedResults.Created("Course created successfully");
+    }
 
     [HttpPost("add-student")]
     public async Task<IResult> AddStudentToCourse(
         [FromQuery] string courseName,
         [FromQuery] string studentRut
-    ) { }
+    )
+    {
+        await _courseRepository.AddStudentToCourseAsync(courseName, studentRut);
+
+        if (await _courseRepository.SaveChangesAsync())
+        {
+            return TypedResults.Created("Student added to course");
+        }
+
+        return TypedResults.BadRequest("Error adding student to course");
+    }
 
     [HttpGet("{name}")]
-    public async Task<IResult> GetCourseByName(string name) { }
+    public async Task<IResult> GetCourseByName(string name)
+    {
+        var course = await _courseRepository.GetCourseByNameAsync(name);
+        if (course == null)
+        {
+            return TypedResults.NotFound("Course not found");
+        }
+
+        return TypedResults.Ok(course);
+    }
 }
