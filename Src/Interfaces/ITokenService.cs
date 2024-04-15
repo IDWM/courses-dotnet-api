301ef9e0a6e3daa6d6bf9c33b80094d1cc8329a1
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-04-14 21:58:59 -0400

    feat: add role name to jwt

--- Src/Interfaces/ITokenService.cs
+++ Src/Interfaces/ITokenService.cs
@@ -2,5 +2,5 @@ namespace courses_dotnet_api.Src.Interfaces;
 
 public interface ITokenService
 {
-    string CreateToken(string rut);
+    string CreateToken(string rut, string nameRole);
 }
--- Src/Services/TokenService.cs
+++ Src/Services/TokenService.cs
@@ -6,29 +6,29 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace courses_dotnet_api.Src.Services;
 
-public class TokenService : ITokenService
+public class TokenService(IConfiguration config) : ITokenService
 {
-    private readonly SymmetricSecurityKey _key;
-
-    public TokenService(IConfiguration config)
-    {
-        _key = new SymmetricSecurityKey(
+    private readonly SymmetricSecurityKey _key =
+        new(
             Encoding.UTF8.GetBytes(
                 config["TokenKey"] ?? throw new InvalidOperationException("Token key not found")
             )
         );
-    }
 
-    public string CreateToken(string rut)
+    public string CreateToken(string rut, string nameRole)
     {
-        var claims = new List<Claim> { new(JwtRegisteredClaimNames.NameId, rut), };
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier, rut),
+            new(ClaimTypes.Role, nameRole)
+        };
 
         var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
 
         var tokenDescriptor = new SecurityTokenDescriptor
         {
             Subject = new ClaimsIdentity(claims),
-            Expires = DateTime.Now.AddDays(7),
+            Expires = DateTime.UtcNow.AddMinutes(30),
             SigningCredentials = creds
         };
 
