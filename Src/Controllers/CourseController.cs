11db64ffad7d2650732e7cc2a110c8fbf51a0b1d
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-04-15 14:12:50 -0400

    fix: Delete methods

--- Src/Controllers/CourseController.cs
+++ Src/Controllers/CourseController.cs
@@ -13,64 +13,14 @@ public class CourseController(ICourseRepository courseRepository) : BaseApiContr
     private readonly string[] _roles = ["admin", "teacher"];
 
     [HttpPost]
-    public async Task<IResult> CreateCourse(CreateCourseDto createCourseDto)
-    {
-        if (!_roles.Contains(User.GetUserRole()))
-        {
-            return TypedResults.Unauthorized();
-        }
-
-        if (await _courseRepository.CourseExistsByNameAsync(createCourseDto.Name))
-        {
-            return TypedResults.BadRequest("Course already exists");
-        }
-
-        await _courseRepository.AddCourseAsync(createCourseDto);
-
-        if (await _courseRepository.SaveChangesAsync())
-        {
-            return TypedResults.Created("Course created successfully");
-        }
-
-        return TypedResults.BadRequest("Failed to create course");
-    }
+    public async Task<IResult> CreateCourse(CreateCourseDto createCourseDto) { }
 
     [HttpPost("add-student")]
     public async Task<IResult> AddStudentToCourse(
         [FromQuery] string courseName,
         [FromQuery] string studentRut
-    )
-    {
-        if (!_roles.Contains(User.GetUserRole()))
-        {
-            return TypedResults.Unauthorized();
-        }
-
-        await _courseRepository.AddStudentToCourseAsync(courseName, studentRut);
-
-        if (await _courseRepository.SaveChangesAsync())
-        {
-            return TypedResults.Created("Student added to course successfully");
-        }
-
-        return TypedResults.BadRequest("Failed to add student to course");
-    }
+    ) { }
 
     [HttpGet("{name}")]
-    public async Task<IResult> GetCourseByName(string name)
-    {
-        if (!_roles.Contains(User.GetUserRole()))
-        {
-            return TypedResults.Unauthorized();
-        }
-
-        CourseDto? course = await _courseRepository.GetCourseByNameAsync(name);
-
-        if (course is null)
-        {
-            return TypedResults.NotFound("Course not found");
-        }
-
-        return TypedResults.Ok(course);
-    }
+    public async Task<IResult> GetCourseByName(string name) { }
 }
