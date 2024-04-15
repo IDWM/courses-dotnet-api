57338f3c3ef73ef205e4acd665c2a011924f7b67
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-04-14 22:03:30 -0400

    feat: add new mappers

--- Src/Helpers/AutoMapperProfiles.cs
+++ Src/Helpers/AutoMapperProfiles.cs
@@ -1,5 +1,7 @@
 using AutoMapper;
 using courses_dotnet_api.Src.DTOs.Account;
+using courses_dotnet_api.Src.DTOs.Course;
+using courses_dotnet_api.Src.DTOs.User;
 using courses_dotnet_api.Src.Models;
 
 namespace courses_dotnet_api.Src.Helpers;
@@ -9,5 +11,8 @@ public class AutoMapperProfiles : Profile
     public AutoMapperProfiles()
     {
         CreateMap<User, CredentialDto>();
+        CreateMap<CreateCourseDto, Course>();
+        CreateMap<Course, CourseDto>();
+        CreateMap<User, UserDto>();
     }
 }
